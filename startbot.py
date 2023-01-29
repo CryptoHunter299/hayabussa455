18b374f8e6ce79b433ade1a1e01f812f6baa9fab
CryptoHunter299 <96936412+CryptoHunter299@users.noreply.github.com>
2023-01-29 19:38:19 +0700

    Update startbot.py

--- startbot.py
+++ startbot.py
@@ -1,1 +1,16 @@
+from telebot import TeleBot
 
+# inisialisasi Token Bot Kita
+bot = TeleBot('6095122638:AAGmzSmgqcs7dhGAd_Ohe_>
+print('Done')
+# Menghandle Pesan /start
+@bot.message_handler(regexp='#testaja')
+def welcome(message):
+    # membalas pesan
+    bot.reply_to(message, 'Halo bro, ada apa?')
+
+while True:
+    try:
+        bot.polling()
+    except:
+        pass
